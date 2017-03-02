7b929fb685d44471f2a2e7f4af71ed043e0d252a
luventa <silentmic@qq.com>
2017-03-02 14:52:59 +0800

    bump v1.0.2: remove ES6 syntax

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const validate = binding => {
+function validate(binding) {
   if (typeof binding.value !== 'function') {
-    let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function.` 
-    console.warn(warn)
+    console.warn('[Vue-click-outside:] provided expression', binding.expression, 'is not a function.')
     return false
   }
 
   return true
 }
 
-const isPopup = (popupItem, elements) => {
+function isPopup(popupItem, elements) {
   if (!popupItem) 
     return false
 
-  for (let element of elements) {
-    if (popupItem.contains(element)) 
+  for (var i = 0, len = elements.length; i < len; i++) {
+    if (popupItem.contains(elements[i])) 
       return true
-    if (element.contains(popupItem))
+    if (elements[i].contains(popupItem))
       return false
   }
 
   return false
 }
 
-export default {
-  bind(el, binding, vNode) {
+exports = module.exports = {
+  bind: function (el, binding, vNode) {
     if (!validate(binding)) return
 
     // Define Handler and cache it on the element
-    const handler = (e) => {
+    function handler(e) {
       if (!vNode.context) return
 
       // some components may have related popup item, on which we shall prevent the click outside event handler.
-      let elements = e.composedPath()
+      var elements = e.composedPath()
       elements.unshift(e.target)    
       
       if (el.contains(e.target) || isPopup(vNode.context.popupItem, elements)) return
@@ -44,11 +43,11 @@ export default {
     document.addEventListener('click', handler)
   },
 
-  update(el, binding) {
+  update: function (el, binding) {
     if (validate(binding)) el.__vueClickOutside__ = binding.value
   },
   
-  unbind(el, binding) {
+  unbind: function (el, binding) {
     // Remove Event Listeners
     document.removeEventListener('click', el.__vueClickOutside__)
     delete el.__vueClickOutside__
