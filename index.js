f034056e2e185daa5258033b38ec7f128463871a
Fangdun Cai <cfddream@gmail.com>
2020-04-15 11:21:23 +0800

    fmt

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ exports = module.exports = {
       // some components may have related popup item, on which we shall prevent the click outside event handler.
       var elements = e.path || (e.composedPath && e.composedPath())
       elements && elements.length > 0 && elements.unshift(e.target)
-      
+
       if (el.contains(e.target) || isPopup(vNode.context.popupItem, elements)) return
 
       el.__vueClickOutside__.callback(e)
@@ -60,7 +60,7 @@ exports = module.exports = {
   update: function (el, binding) {
     if (validate(binding)) el.__vueClickOutside__.callback = binding.value
   },
-  
+
   unbind: function (el, binding, vNode) {
     // Remove Event Listeners
     const clickHandler = 'ontouchstart' in document.documentElement ? 'touchstart' : 'click';
